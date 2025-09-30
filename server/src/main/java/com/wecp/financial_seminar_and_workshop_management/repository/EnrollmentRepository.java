c4cc131d9055e38742e1deeb2af4559e60d32488
username <wecp-dummy@gmail.com>
2025-09-30 12:16:14 +0000

    Service Added

--- server/src/main/java/com/wecp/financial_seminar_and_workshop_management/repository/EnrollmentRepository.java
+++ server/src/main/java/com/wecp/financial_seminar_and_workshop_management/repository/EnrollmentRepository.java
@@ -3,8 +3,6 @@ package com.wecp.financial_seminar_and_workshop_management.repository;
 import com.wecp.financial_seminar_and_workshop_management.entity.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
-public interface EnrollmentRepository {
-    // implement repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long>{
 }
--- server/src/main/java/com/wecp/financial_seminar_and_workshop_management/service/UserService.java
+++ server/src/main/java/com/wecp/financial_seminar_and_workshop_management/service/UserService.java
@@ -3,6 +3,7 @@ package com.wecp.financial_seminar_and_workshop_management.service;
 import com.wecp.financial_seminar_and_workshop_management.entity.User;
 import com.wecp.financial_seminar_and_workshop_management.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
