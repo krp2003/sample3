34200a3659d7edbeb2f0fd7767075b4c3d3d16de
username <wecp-dummy@gmail.com>
2025-10-04 09:36:31 +0000

    App Component update

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
   IsLoggin:any=false;
   roleName: string | null;
-  constructor(private authService: AuthService, private router:Router)
+  constructor(public authService: AuthService, private router:Router)
   {
    
     this.IsLoggin=authService.getLoginStatus;
